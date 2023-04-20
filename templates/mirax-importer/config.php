70886d3ff4ea68f2f0990de22a99675d64fa81c1
Aiosa <horakj7@gmail.com>
2023-04-20 12:07:16 +0200

    New values for importer configuration file

--- templates/mirax-importer/config.php
+++ templates/mirax-importer/config.php
@@ -1,4 +1,11 @@
 <?php
+
+function _read_env_mirax_importer($name, $default) {
+    $value = getenv($name);
+    if (empty($value) || $value == "") return $default;
+    return $value;
+}
+
 //relative path to xo_db interface
 const XO_DB_ROOT = "../../xo_db/";
 //where we start uploading
@@ -12,3 +19,5 @@ $analysis_event_name=function ($event) {
     if ($event === "mirax-importer") return $event;
     return "histopipe_$event";
 };
+$mirax_pattern = "/^(.*([0-9]{4})[_-]([0-9]+).*)\.mrxs?$/i";
+$server_api_url = _read_env_mirax_importer("XO_MIRAX_SERVER_URL", "http://localhost:8081/importer/server");
