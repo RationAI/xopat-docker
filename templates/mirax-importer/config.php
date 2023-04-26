242889262bd077ad68f4848df7a819e2f533ec98
Aiosa <horakj7@gmail.com>
2023-04-26 19:50:56 +0200

    wrong regex

--- templates/mirax-importer/config.php
+++ templates/mirax-importer/config.php
@@ -6,18 +6,17 @@ function _read_env_mirax_importer($name, $default) {
     return $value;
 }
 
-//relative path to xo_db interface
 const XO_DB_ROOT = "../../xo_db/";
-//where we start uploading
 $upload_root = "/var/www/data/";
+$safe_mode = false; //e.g. deleting all for testing purposes
 $log_file = "$upload_root/log.txt";
-//where are processing scripts
 $server_root = "/var/www/html/importer/server/";
-//for database, how events are named
 $analysis_event_name=function ($event) {
     if (!$event) throw new Exception("Event name must be defined! Got '$event'.");
     if ($event === "mirax-importer") return $event;
     return "histopipe_$event";
 };
-$mirax_pattern = "/^(.*([0-9]{4})[_-]([0-9]+).*)\.mrxs?$/i";
+//must have three groups: 1) capture file name without extension 2) capture year 3) capture biopsy
+$mirax_pattern = "/^(.*?([0-9]{4})[_-]([0-9]+).*)\.mrxs$/i";
+//the server URL where it is deployed ($server_api_url/config.php is this file)
 $server_api_url = _read_env_mirax_importer("XO_MIRAX_SERVER_URL", "http://localhost:8081/importer/server");
