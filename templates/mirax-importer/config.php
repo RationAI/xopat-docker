2c5879d51af2be0f56be5ff7cb6aeaf48b4b730a
Aiosa <horakj7@gmail.com>
2023-04-27 16:02:44 +0200

    Add copy ownership for www-data, new mirax-importer config.

--- templates/mirax-importer/config.php
+++ templates/mirax-importer/config.php
@@ -8,15 +8,14 @@ function _read_env_mirax_importer($name, $default) {
 
 const XO_DB_ROOT = "../../xo_db/";
 $upload_root = "/var/www/data/";
-$safe_mode = false; //e.g. deleting all for testing purposes
+$safe_mode = true; //allow deleting all for testing purposes
 $log_file = "$upload_root/log.txt";
+$analysis_log_file = "$upload_root/analysis_log.txt";
 $server_root = "/var/www/html/importer/server/";
-$analysis_event_name=function ($event) {
-    if (!$event) throw new Exception("Event name must be defined! Got '$event'.");
-    if ($event === "mirax-importer") return $event;
-    return "histopipe_$event";
-};
+//event name used for the importer instance (do not change at runtime)
+$importer_own_event = "mirax-importer";
 //must have three groups: 1) capture file name without extension 2) capture year 3) capture biopsy
 $mirax_pattern = "/^(.*?([0-9]{4})[_-]([0-9]+).*)\.mrxs$/i";
 //the server URL where it is deployed ($server_api_url/config.php is this file)
 $server_api_url = _read_env_mirax_importer("XO_MIRAX_SERVER_URL", "http://localhost:8081/importer/server");
+$run_conversion_as_job = _read_env_mirax_importer("XO_MIRAX_KUBERNETES", false);
