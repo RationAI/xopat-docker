370a4a13e68aeb5f96a9486e62fb44d20d962813
Aiosa <horakj7@gmail.com>
2023-04-29 10:15:14 +0200

    Init scripts - include git pull for up to date content, disable htaccess for the time being

--- templates/mirax-importer/docker-entrypoint.sh
+++ templates/mirax-importer/docker-entrypoint.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
-# Directories created by the docker image before.
-AUTH_FILE="/usr/local/etc/apache/.htpasswd"
+# # Directories created by the docker image before.
+# Todo: disabled due to kubernetes
+# AUTH_FILE="/usr/local/etc/apache/.htpasswd"
+# if [ -z $XO_BASIC_USER ]; then
+#     echo "Authentication disabled."
+# elif [ -s $AUTH_FILE ]; then
+#     echo "Created authentication" \
+#        && htpasswd -bc $AUTH_FILE $XO_BASIC_USER "$XO_BASIC_PWD" \
+#        && echo "SetEnv XO_BASIC_AUTH 5" >> /etc/apache2/.env.apache
+# else
+#     echo "Authentication file exists - skipping..."
+# fi
 
-if [ -z $XO_BASIC_USER ]; then
-    echo "Authentication disabled."
-elif [ -s $AUTH_FILE ]; then
-    echo "Created authentication" \
-       && htpasswd -bc $AUTH_FILE $XO_BASIC_USER "$XO_BASIC_PWD" \
-       && echo "SetEnv XO_BASIC_AUTH 5" >> /etc/apache2/.env.apache
-else
-    echo "Authentication file exists - skipping..."
-fi
+# Keep up to date.
+cd /var/www/html/importer && git pull
+
 apache2ctl -D FOREGROUND
